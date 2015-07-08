3db3f5d2aad887c9b06c5381c6a8a7b5b1ec7985
Norman Feske <norman.feske@genode-labs.com>
2015-07-08 18:40:05 +0200

    CPU load display: highlight hovered timeline (WiP)

--- repos/gems/src/app/cpu_load_display/main.cc
+++ repos/gems/src/app/cpu_load_display/main.cc
@@ -56,7 +56,7 @@ class Cpu_load_display::Timeline : public Genode::List<Timeline>::Element
 
 		unsigned _sum_saved_activity = 0;
 
-		unsigned _saved_now = 0;
+		bool _hovered = false;
 
 		Label _label;
 
@@ -100,15 +100,12 @@ class Cpu_load_display::Timeline : public Genode::List<Timeline>::Element
 				_saved_activity[i] = _activity[i];
 
 			_sum_saved_activity = _sum_activity;
-			_saved_now = now;
 		}
 
 		unsigned long saved_activity(unsigned i) const
 		{
 			return _saved_activity[i % HISTORY_LEN];
 		}
-
-		unsigned saved_now() const { return _saved_now; }
 
 		bool has_subject_id(unsigned subject_id) const
 		{
@@ -129,13 +126,17 @@ class Cpu_load_display::Timeline : public Genode::List<Timeline>::Element
 
 		Color color(Color_type type) const
 		{
-			unsigned const brightness = 140;
+			unsigned const brightness = _hovered ? 220 : 140;
 			unsigned const saturation = type == COLOR_TOP ? 70 : 140;
 			unsigned const alpha      = 230;
 
 			Color const c = color_from_hsv(_hue(), saturation, brightness);
 			return Color(c.r, c.g, c.b, alpha);
 		}
+
+		unsigned subject_id() const { return _subject_id; }
+
+		void hovered(bool hovered) { _hovered = hovered; }
 };
 
 
@@ -145,6 +146,8 @@ class Cpu_load_display::Cpu : public Genode::List<Cpu>::Element
 
 		Genode::Point<>  const _pos;
 		Genode::List<Timeline> _timelines;
+
+		unsigned _saved_now = 0;
 
 		Timeline *_lookup_timeline(Xml_node subject)
 		{
@@ -223,13 +226,24 @@ class Cpu_load_display::Cpu : public Genode::List<Cpu>::Element
 		{
 			for (Timeline *t = _timelines.first(); t; t = t->next())
 				t->save(now);
+
+			_saved_now = now;
 		}
+
+		unsigned saved_now() const { return _saved_now; }
 
 		template <typename FN>
 		void for_each_timeline(FN const &fn) const
 		{
 			for (Timeline const *t = _timelines.first(); t; t = t->next())
 				fn(*t);
+		}
+
+		void hovered(unsigned subject_id, bool hovered)
+		{
+			for (Timeline *t = _timelines.first(); t; t = t->next())
+				if (t->has_subject_id(subject_id))
+					t->hovered(hovered);
 		}
 };
 
@@ -299,6 +313,12 @@ class Cpu_load_display::Cpu_registry
 			for (Cpu *cpu = _cpus.first(); cpu; cpu = cpu->next())
 				cpu->advance(now);
 		}
+
+		void hovered(unsigned subject_id, bool hovered)
+		{
+			for (Cpu *cpu = _cpus.first(); cpu; cpu = cpu->next())
+				cpu->hovered(subject_id, hovered);
+		}
 };
 
 
@@ -345,6 +365,8 @@ class Cpu_load_display::Scene : public Nano3d::Scene<PT>,
 
 		bool _hovered = false;
 
+		unsigned _hovered_subject_id = 0;
+
 		long _activity_sum[Timeline::HISTORY_LEN];
 		long _y_level[Timeline::HISTORY_LEN];
 		long _y_curr[Timeline::HISTORY_LEN];
@@ -379,7 +401,7 @@ class Cpu_load_display::Scene : public Nano3d::Scene<PT>,
 				Color const top_color    = timeline.color(Timeline::COLOR_TOP);
 				Color const bottom_color = timeline.color(Timeline::COLOR_BOTTOM);
 
-				unsigned timeline_now = timeline.saved_now();
+				unsigned const timeline_now = cpu.saved_now();
 
 				for (unsigned i = 0; i < HISTORY_LEN; i++) {
 
@@ -433,6 +455,33 @@ class Cpu_load_display::Scene : public Nano3d::Scene<PT>,
 			});
 		}
 
+		/**
+		 * Apply functor to each visible CPU
+		 *
+		 * The functor is called with the geomery of the graph and the CPU
+		 * as arguments.
+		 */
+		template <typename FN>
+		void _for_each_visible_cpu(FN const &fn)
+		{
+			/* determine number of CPUs */
+			unsigned num_cpus = 0;
+			_cpu_registry.for_each_cpu([&] (Cpu const &cpu) { num_cpus++; });
+			if (num_cpus == 0)
+				return;
+
+			/* visit graphs for the CPUs below each other */
+			enum { GAP = 8 };
+			Nitpicker::Point const step(0, _size.h()/num_cpus);
+			Nitpicker::Area  const size(_size.w(), step.y() - GAP);
+			Nitpicker::Point       point(0, GAP/2);
+
+			_cpu_registry.for_each_cpu([&] (Cpu const &cpu) {
+				fn(Nitpicker::Rect(point, size), cpu);
+				point = point + step;
+			});
+		}
+
 	public:
 
 		Scene(Genode::Signal_receiver &sig_rec, unsigned update_rate_ms,
@@ -449,7 +498,6 @@ class Cpu_load_display::Scene : public Nano3d::Scene<PT>,
 
 			Nano3d::Scene<PT>::input_handler(this);
 		}
-
 
 		/**
 		 * Scene interface
@@ -479,23 +527,69 @@ class Cpu_load_display::Scene : public Nano3d::Scene<PT>,
 			points[3] = Point(0,     h - 1, bottom_color);
 			_shaded_painter.paint(pixel, alpha, points, 4);
 
-			/* determine number of CPUs */
-			unsigned num_cpus = 0;
-			_cpu_registry.for_each_cpu([&] (Cpu const &cpu) { num_cpus++; });
-
-			if (num_cpus == 0)
-				return;
-
-			/* plot graphs for the CPUs below each other */
-			enum { GAP = 8 };
-			Nitpicker::Point const step(0, _size.h()/num_cpus);
-			Nitpicker::Area  const size(_size.w(), step.y() - GAP);
-			Nitpicker::Point       point(0, GAP/2);
-
-			_cpu_registry.for_each_cpu([&] (Cpu const &cpu) {
-				_plot_cpu(pixel, alpha, cpu, Nitpicker::Rect(point, size));
-				point = point + step;
+			_for_each_visible_cpu([&] (Nitpicker::Rect rect, Cpu const &cpu) {
+				_plot_cpu(pixel, alpha, cpu, rect);
 			});
+		}
+
+		void _handle_pointer_at(Nitpicker::Point pos)
+		{
+			unsigned subject_id_at_pos = 0;
+
+			/* determine the subject ID at the specified position */
+			_for_each_visible_cpu([&] (Nitpicker::Rect rect, Cpu const &cpu) {
+
+				if (!rect.contains(pos))
+					return;
+
+				/* stop search once we found the subject at the position */
+				if (subject_id_at_pos)
+					return;
+
+				/* shortcuts */
+				enum { HISTORY_LEN = Timeline::HISTORY_LEN,
+				       XSTEPS      = HISTORY_LEN - 1};
+
+				/* position relative to the graph of the current CPU */
+				unsigned const dx = pos.x() - rect.x1();
+				unsigned const dy = rect.h() - (pos.y() - rect.y1() + 1);
+
+				/* determine index 't' within the timelines */
+				unsigned const saved_now = cpu.saved_now();
+				unsigned const i = XSTEPS - 1 - (XSTEPS*dx) / rect.w();
+				unsigned const t = (saved_now - i) % HISTORY_LEN;
+
+				/*
+				 * Now that we have found the point in time, we need
+				 * to accumulate the the timeline heights at this point
+				 * until we cross the y coordinate of the position.
+				 */
+				unsigned const activity_sum = cpu.activity_sum(t);
+				if (activity_sum == 0)
+					return;
+
+				/* accumulated timeline heights */
+				unsigned activity_level_dy = 0;
+
+				cpu.for_each_timeline([&] (Timeline const &timeline) {
+
+				 	/* search no longer once we found the timeline */
+					if (subject_id_at_pos)
+						return;
+
+					activity_level_dy +=
+						(timeline.saved_activity(t)*rect.h()) / activity_sum;
+
+					if (activity_level_dy >= dy)
+						subject_id_at_pos = timeline.subject_id();
+				});
+			});
+
+			if (_hovered_subject_id != subject_id_at_pos) {
+				_cpu_registry.hovered(_hovered_subject_id, false);
+				_hovered_subject_id = subject_id_at_pos;
+				_cpu_registry.hovered(_hovered_subject_id, true);
+			}
 		}
 
 		/**
@@ -503,17 +597,20 @@ class Cpu_load_display::Scene : public Nano3d::Scene<PT>,
 		 */
 		void handle_input(Input::Event const events[], unsigned num) override
 		{
-
 			for (unsigned i = 0; i < num; i++) {
 
 				Input::Event const &ev = events[i];
 
 				if (ev.type() == Input::Event::MOTION) {
 					_hovered = true;
+
+					_handle_pointer_at(Nitpicker::Point(ev.ax(), ev.ay()));
 				}
 
 				if (ev.type() == Input::Event::LEAVE) {
 					_hovered = false;
+					_cpu_registry.hovered(_hovered_subject_id, false);
+					_hovered_subject_id = 0;
 				}
 			}
 		}
