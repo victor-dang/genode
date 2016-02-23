2b9202db267fd33f55ac0d56760cf6c3ed3d8501
Stefan Kalkowski <stefan.kalkowski@genode-labs.com>
2016-02-23 14:29:13 +0100

    audio_mixer: use bool for mute XML report parsing
    Fix #2123

--- repos/os/include/mixer/channel.h
+++ repos/os/include/mixer/channel.h
@@ -54,7 +54,7 @@ struct Mixer::Channel
 			number = (Channel::Number) node.attribute_value<long>("number", 0);
 			volume = node.attribute_value<long>("volume", 0);
 			active = node.attribute_value<long>("active", 0);
-			muted  = node.attribute_value<long>("muted", 0);
+			muted  = node.attribute_value<bool>("muted", true);
 		} catch (...) { throw Invalid_channel(); }
 	}
 };
