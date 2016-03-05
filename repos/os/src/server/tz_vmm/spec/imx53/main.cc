3e7991b618c1abd49d4ade9e2e41b82c626cb15f
jonhedave <johndv2123@gmail.com>
2016-03-05 02:07:14 +0100

    Update main.cc

--- repos/os/src/server/tz_vmm/spec/imx53/main.cc
+++ repos/os/src/server/tz_vmm/spec/imx53/main.cc
@@ -35,7 +35,7 @@ enum {
 };
 
 
-static const char* cmdline_tablet = "console=ttymxc0,115200";
+static const char* cmdline_tablet = "video=mxcdi0fb:RGB666,XGA di0_primary ldb=di0 earlyprintk loglevel=10";
 
 void on_vmm_entry();
 void on_vmm_exit();
