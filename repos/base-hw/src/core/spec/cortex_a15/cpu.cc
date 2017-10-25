84342c0ad20d7a5cb433b38785c1730a44dcf2e3
Stefan Kalkowski <stefan.kalkowski@genode-labs.com>
2017-10-25 18:57:58 +0200

    hw: unify mmu fault handling
    Recent work related to issue 1723 showed that there is potential
    to get rid of code duplication in MMU fault handling especially
    with regard to ARM cpus.

--- repos/base-hw/src/core/spec/cortex_a15/cpu.cc
+++ repos/base-hw/src/core/spec/cortex_a15/cpu.cc
@@ -14,6 +14,7 @@
 #include <util/bit_allocator.h>
 #include <base/internal/unmanaged_singleton.h>
 
+#include <kernel/thread.h>
 #include <spec/cortex_a15/cpu.h>
 
 using Asid_allocator = Genode::Bit_allocator<256>;
@@ -25,3 +26,22 @@ static Asid_allocator &alloc() {
 Genode::Cpu::Mmu_context::Mmu_context(addr_t table)
 : ttbr0(Ttbr_64bit::Ba::masked((Ttbr_64bit::access_t)table)) {
 	Ttbr_64bit::Asid::set(ttbr0, (Genode::uint8_t)alloc().alloc()); }
+
+
+void Genode::Cpu::mmu_fault_status(Genode::Cpu::Fsr::access_t fsr,
+                                   Kernel::Thread_fault & fault)
+{
+	enum {
+		FAULT_MASK  = 0b111100,
+		TRANSLATION = 0b000100,
+		PERMISSION  = 0b001100,
+	};
+
+	using Fault = Kernel::Thread_fault;
+
+	switch(fsr & FAULT_MASK) {
+	case TRANSLATION: fault.type = Fault::PAGE_MISSING; return;
+	case PERMISSION:  fault.type = Fault::PERMISSION;   return;
+	default:          fault.type = Fault::UNKNOWN;
+	};
+};
